78c3682f712b7b3c3d354c1fed4c534960958bed
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-06-16 09:47:12 +0200

    miglioramenti

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,34 +4,6 @@
 
 @section('mainContent')
     <main>
-        {{-- <div class="container">
-        <h1>Fumetti</h1>
-        <table class="table table-borderless">
-            <thead>
-              <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Titolo</th>
-                <th scope="col">Serie</th>
-                <th scope="col">Tipo</th>
-                <th scope="col">Prezzo</th>
-                <th scope="col">Visualizza</th>
-              </tr>
-            </thead>
-            <tbody>
-            @foreach ($prodotti as $prodotto)
-              <tr>
-                <th scope="row">{{$prodotto->id}}</th>
-                <td>{{$prodotto->title}}</td>
-                <td>{{$prodotto->series}}</td>
-                <td>{{$prodotto->type}}</td>
-                <td>{{$prodotto->price}}</td>
-                <td><a href="{{route('products.show',$prodotto->id)}}" class="btn btn-info">Visualizza</a></td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-          
-        </div> --}}
           <div class="jumbotron">
               <img src="{{asset('../img/jumbotron.jpg')}}" alt="jumbotron">
           </div>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -4,16 +4,6 @@
 
 @section('mainContent')
     <main>
-        {{-- <div class="container">
-        <h1 class="text-center">{{$product->title}}</h1>
-        <div class="d-flex justify-content-center">
-            <img src="{{$product->image}}" alt="{{$product->title}}">
-            <p class="p-2">{{$product->description}} <br>
-                Data di vendita: {{$product->sale_date}}
-            </p>
-        </div>
-        <div><a href="{{route('products.index')}}" class="btn btn-info">Ritorna ai fumetti</a></div>
-        </div> --}}
         <div class="jumbotron">
             <img src="{{asset('../img/jumbotron.jpg')}}" alt="">
             <div class="image-hover">
